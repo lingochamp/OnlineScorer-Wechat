9a8642248829134c85a3feb3e3ce369215a24763
Rebecca Lu <lbjrebecca@gmail.com>
2017-04-26 13:19:42 +0800

    remove example code

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/dist'));
-
-var server = app.listen(80, function() {
-  var port = server.address().port;
-
-  console.log("mock server started at http://localhost:" + port);
-});
