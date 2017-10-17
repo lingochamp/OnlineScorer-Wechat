14df0f125b15a61cce9e747aa8542d76071cf80f
Rebecca Lu <lbjrebecca@gmail.com>
2017-10-17 19:15:10 +0800

    update gulp, use semantic release

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,9 @@ const babel = require('gulp-babel');
 const browserify = require('gulp-browserify');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
-const eslint = require('gulp-eslint');
 const runSequence = require('run-sequence');
-const del = require('del');
 
 const version = '1.1.1';
-
-gulp.task('lint', () => {
-  return gulp.src('src/**').pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failOnError());
-});
 
 gulp.task('browserify', () => {
   return gulp.src(`lib/index.js`)
@@ -34,10 +26,6 @@ gulp.task('build', () => {
     .pipe(gulp.dest('./lib'));
 });
 
-gulp.task('build-clean', () => {
-  return del(['lib', 'dist']);
-});
-
 gulp.task('default', cb => {
-  runSequence('build-clean', `build`, `browserify`, cb);
+  runSequence(`build`, `browserify`, cb);
 });
